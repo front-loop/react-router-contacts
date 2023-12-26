5e9a4a91443a989ab08f40178ba914248e9afacd
mancuoj <1337381768@qq.com>
2023-12-27 00:10:16 +0800

    feat: error page

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Root from './routes/root'
 import './index.css'
+import Root from './routes/root'
+import ErrorPage from './routes/error-page'
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Root />
-  }
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
 ])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
---
+++ src/routes/error-page.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react'
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom'
+
+const ErrorPage: FC = () => {
+  const error = useRouteError()
+  let errorText = <p>Unknown Error</p>
+
+  if (isRouteErrorResponse(error)) {
+    errorText = (
+      <p>
+        {error.status} {error.statusText}
+      </p>
+    )
+  }
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      {errorText}
+    </div>
+  )
+}
+
+export default ErrorPage
