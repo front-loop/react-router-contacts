6134a4902f60cb2f7e157b55686c1de1f23636a7
mancuoj <1337381768@qq.com>
2024-01-01 21:07:25 +0800

    feat: jsx route

--- src/main.tsx
+++ src/main.tsx
@@ -43,6 +43,20 @@ const router = createBrowserRouter([
   },
 ])
 
+// 另一种写法 JSX
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<Root />} loader={rootLoader} action={rootAction} errorElement={<ErrorPage />}>
+//       <Route errorElement={<ErrorPage />}>
+//         <Route index element={<Index />} />
+//         <Route path="contacts/:contactId" element={<Contact />} loader={contactLoader} action={contactAction} />
+//         <Route path="contacts/:contactId/edit" element={<EditContact />} loader={contactLoader} action={editAction} />
+//         <Route path="contacts/:contactId/destroy" action={destroyAction} />
+//       </Route>
+//     </Route>
+//   )
+// )
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
