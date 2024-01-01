8a6dd32adc3b1c2190406521d00012aae00e95c6
mancuoj <1337381768@qq.com>
2024-01-01 20:31:05 +0800

    feat: destroy page

---
+++ src/routes/destroy.tsx
@@ -0,0 +1,7 @@
+import { LoaderFunction, redirect } from 'react-router-dom'
+import { deleteContact } from '../contacts'
+
+export const action = (async ({ params }) => {
+  await deleteContact(params.contactId!)
+  return redirect('/')
+}) satisfies LoaderFunction
